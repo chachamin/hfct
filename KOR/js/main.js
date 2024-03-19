c199eef939138025c8e17da4654ca6ea26403024
chachamin <wlauswlaus@hanmail.net>
2024-03-19 14:35:31 +0900

    메인 레이아웃 변경

--- KOR/js/main.js
+++ KOR/js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
     $(window).scroll(function () {
-        if ($(document).scrollTop() > 3450) {
+        if ($(document).scrollTop() > 1300) {
             $(".global_quick").addClass("db_none");
         } else {
             $(".global_quick").removeClass("db_none");
         }
     });
+
 
 });
